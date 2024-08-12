823e3a7f1b605f602c8d415d568593634b72c1c8
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-12 16:41:01 +0300

    Add project files.

---
+++ PolymorphismExample/BaseGeometrikSekil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExample
+{
+
+    public class BaseGeometrikSekil()
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+
+        public virtual int AlanHesapla(int genislik, int yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+            int alan = genislik * yukseklik;
+            Console.WriteLine(alan);
+            return alan;
+
+        }
+    }
+
+    public class Kare : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            return base.AlanHesapla(genislik, yukseklik);
+        }
+    }
+
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            return base.AlanHesapla(genislik, yukseklik);
+        }
+    }
+
+    public class Ucgen : BaseGeometrikSekil
+    {
+        public override int AlanHesapla(int genislik, int yukseklik)
+        {
+            int alan = (genislik * yukseklik) / 2;
+            Console.WriteLine(alan);
+            return alan;
+        }
+    }
+
+}
---
+++ PolymorphismExample/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using PolymorphismExample;
+
+BaseGeometrikSekil kare=new Kare();
+
+kare.AlanHesapla(10, 2);
+
+BaseGeometrikSekil dikdortgen = new Dikdortgen();
+
+dikdortgen.AlanHesapla(10, 8);
+
+BaseGeometrikSekil ucgen = new Ucgen();
+
+ucgen.AlanHesapla(10, 7);
